f3a1874f06044cacb4ddf3df1d63800f37396c3a
Constance Royal <constance.royal@gmail.com>
2019-05-03 14:35:17 +0200

    connection debut

---
+++ LimiteLimite/Limite-web/src/main/java/fr/formation/controller/ConnectionController.java
@@ -0,0 +1,16 @@
+package fr.formation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/connection")
+public class ConnectionController {
+
+
+	
+//	@GetMapping("/")
+//	public String connection(Model model, @RequestParam int id) {
+//	
+//		return "";
+}
